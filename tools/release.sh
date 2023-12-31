0ee3b1f2dc37bc7fc7236ad3b4e628cd2d09a575
Di Zou <di.zou1214@gmail.com>
2023-12-31 08:45:10 -0500

    update build script

--- tools/release.sh
+++ tools/release.sh
@@ -1,5 +1,6 @@
 rm -rf dist
 npx concurrently "nx run xws:build" "cd apps/core; make all -j 3"
+rm -f dist/apps/XWebStack/mac_arm.xpl dist/apps/XWebStack/mac_amd.xpl
 cp -r dist/apps/xws dist/apps/XWebStack
 echo "PORT=19090" > dist/apps/XWebStack/config
 root=$(pwd)
